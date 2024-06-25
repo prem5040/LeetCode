2babe4e24317ccc42e1c7dbf08e9183b5cdbad65
Prem Kumar Yadav <81683444+prem5040@users.noreply.github.com>
2024-06-25 10:00:34 +0545

    75. Sort Colors LC
    Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
    We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
    You must solve this problem without using the library's sort function.
    Example 1:
    Input: nums = [2,0,2,1,1,0]
    Output: [0,0,1,1,2,2]

---
+++ sortColors.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/sort-colors/submissions/1242932988/
+class Solution {
+    public void sortColors(int[] nums) {
+        int zeroIndex = 0, twoIndex = nums.length - 1, i = 0;
+        while (i <= twoIndex) {
+            if (nums[i] == 2) {
+                nums[i] = nums[twoIndex];
+                nums[twoIndex--] = 2;
+            } else {
+                if (nums[i] == 0) {
+                    nums[i] = nums[zeroIndex];
+                    nums[zeroIndex++] = 0;
+                } 
+                i++;
+            }
+        }
+    }
+}
